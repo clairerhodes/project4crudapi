2b31926205447afd01485ad4fb815d1dcf155bc4
Abdulla Durrani <Abdullahkd@gmail.com>
2025-01-13 17:17:06 -0500

    debugging, home is rendering data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,27 @@
 import { useState } from 'react'
 import './App.css'
 import Home from './components/home/home.jsx'
+import Navbar from './components/nav/Navbar.jsx';
 // need to import authentication, 
 
 const App = () => {
   // state variables
   const [page, setPage] = useState("Home");
   const [content, setContent] = useState([]);
+  const [user, setUser] = useState(null);
+  const [contentId, setContentId] = useState(null);
   
   // function to update page state
-  const setPage = (event) => {
+  const handleSection = (event) => {
     setPage(event.target.value);
   };
 
   return (
     <>
-      <Navbar setPage={setPage} />
+      <Navbar handleSection={handleSection} />
       {/* default show home page */}
         {page === 'Home' ? <Home content={content} setContent={setContent}/> : ''}
+        {/* {page === 'Home' ? <Home/> : ''} */}
       {/* user profile section */}
         {/* do we also want to have page that shows a list of users to click? or just ability to click on a user through their post */}
         {page === "profile" ? <Profile /> : ""}
