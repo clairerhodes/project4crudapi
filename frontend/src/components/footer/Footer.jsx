233c33a10316ddba0f6e2723ce5c05adcfc944cf
ChrisL <caleedev@gmail.com>
2025-01-14 16:24:36 -0500

    made create post redirect to the created posts show page.  Implemented footer.

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -4,10 +4,7 @@ const Footer = () => {
 
     return (
         // this can include info about our team & app on every page
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Saepe iusto necessitatibus hic ea illo. Nam voluptas dolore 
-            veritatis quas, quo saepe sint delectus officia dolorem 
-            voluptates possimus dignissimos libero architecto.</p>
+        <h6>&copy; Shot in the Dark Productions: Abdullah Durrani, Claire Rhodes, Chris Lee</h6>
     )
 }
 
